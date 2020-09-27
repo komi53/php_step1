9809b41158d6d26874a40324168c2523638e8dcb
komi53 <kxs53kuro@gmail.com>
2020-09-27 22:40:57 +0900

    function add

--- app/index.php
+++ app/index.php
@@ -1,1 +1,31 @@
-<?php echo '<p>Hello World</p>'; ?>
+    <?php
+    	$a = 5;
+    	if ($a == 3) {
+    		print("$a is 3");
+    	}
+    	else{
+    		print("$a is not 3");
+    	}
+    ?>
+    <br>
+    <?php
+	    for($i = 0; $i < 10; $i++){
+	    	print("$i");
+	    }
+	?>
+	<br>
+	<?php
+		function double_print($text){
+			print($text . $text);
+		}
+
+		double_print("a");
+		double_print("bc");
+	?>
+	<br>
+	<?php
+		$a1 = array("one", "two", "three");
+		$a1[] = "four";
+		$a1[0] = "one?";
+		print_r($a1);
+	?>
